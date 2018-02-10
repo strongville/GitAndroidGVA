ce22332d0d3e5ce315f24206dbb0273010569fa3
Guillermo Villafuerte <gvillafu@comunidad.unam.mx>
2018-02-10 13:29:30 -0600

    Agregado botón "Puchale aquí"

--- app/src/main/java/mx/unam/aragon/fes/strongvilledev/gitandroid/MainActivity.java
+++ app/src/main/java/mx/unam/aragon/fes/strongvilledev/gitandroid/MainActivity.java
@@ -2,12 +2,28 @@ package mx.unam.aragon.fes.strongvilledev.gitandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnClic;
+    private TextView txtHola;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnClic = findViewById(R.id.btnClic);
+        txtHola = findViewById(R.id.txtHola);
+
+        btnClic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                txtHola.setText(R.string.hola);
+            }
+        });
     }
 }
